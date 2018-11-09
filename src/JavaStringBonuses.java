4cea7945e1ae52c09c2d357f1e175b84c6f9e638
Mark Stachowski <markstachowski@me.com>
2018-11-09 06:26:58 -0600

    Added input validation on Java Date formatting app

--- src/JavaStringBonuses.java
+++ src/JavaStringBonuses.java
@@ -4,23 +4,32 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class JavaStringBonuses {
-    public static void main(String[] args) throws ParseException {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\nEnter a date (MM/DD/YYYY) in the date format converter application:\n");
-        DateFormat parseFormat = new SimpleDateFormat(
-                "MM/dd/yyyy");
-        String inputDate = sc.nextLine();
-        Date date = parseFormat.parse(inputDate);
-//        String strDate = parseFormat.format(date); // Checks date entered
-//        System.out.println(strDate);  // Prints date entered
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "MMMM dd, yyyy");
-        String stringDate = dateFormat.format(date);
-        System.out.println(stringDate);
+        Date date = null;
+        try {
+            System.out.println("\nEnter a date (MM/DD/YYYY) in the date format converter application:\n");
+            DateFormat parseFormat = new SimpleDateFormat(
+                    "MM/dd/yyyy");
+            String inputDate = sc.nextLine();
+            date = parseFormat.parse(inputDate);
+            if (!inputDate.equals(parseFormat.format(date))) {
+                date = null;
+            }
+        } catch (ParseException ex) {
+            ex.printStackTrace(); // NonCompliant, security risk... Default Stream in System.Err
+        }
+        if (date == null) {
+            System.out.println("Invalid date format, use (MM/DD/YYYY)");
+        } else {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(
+                    "MMMM dd, yyyy");
+            String stringDate = dateFormat.format(date);
+            System.out.format("%s is a Valid date format", stringDate);
+        }
 
     }
 }
