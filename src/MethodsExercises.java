ca1ce18857e0f1c930e755184d46c046d51e101d
Mark Stachowski <markstachowski@me.com>
2018-11-12 09:56:38 -0600

    adding more methods exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class MethodsExercises {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
 
 //        System.out.println(adding(5, 10));
@@ -15,8 +14,8 @@ public class MethodsExercises {
 //        System.out.println(modulus(6, 4));
 //        System.out.println(multiplyLoop(9, 9));
 //        System.out.println(factoral(5));
-//        System.out.println(factorialRecur(5));
-        System.out.println(factorialT(5));
+//        System.out.println(factorialRecur(1));
+//        System.out.println(factorialT(5));
 
     }
 
@@ -48,50 +47,60 @@ public class MethodsExercises {
         if (x == 0 || y == 0) {
             return 0;
         } else if (y > 0) {
-            return x + multiplyLoop( (int)x, (int) y - 1);
+            return x + multiplyLoop((int) x, (int) y - 1);
         }
-        return - multiplyLoop( (int) x, (int) -y);
+        return -multiplyLoop((int) x, (int) -y);
     }
 
-    public static int multiplyForLoop (int x, int y) {
-        if(x == 0 || y == 0){
+    public static int multiplyForLoop(int x, int y) {
+        if (x == 0 || y == 0) {
             return 0;
         }
-        if(x < 0) {
+        if (x < 0) {
             return -multiply(-x, y);
         }
         int result = 0;
-        for(int i = x; i > 0; i--){
+        for (int i = x; i > 0; i--) {
             result += y;
         }
         return result;
     }
 
 
-    public static int factoralFor (int num) {
-        int i, fact=1;
+    public static int factoralFor(int num) {
+        int i, fact = 1;
         //int number=5;//It is the number to calculate factorial
-        for(i=1; i <= num; i++) {
-            fact=fact*i;
+        for (i = 1; i <= num; i++) {
+            fact = fact * i;
         }
-        System.out.println("Factorial of "+ num +" is: "+ fact);
+        System.out.println("Factorial of " + num + " is: " + fact);
         return i * fact;
     }
 
-
-    public static int factorialRecur(int num){
-        if (num == 0)
-            return 1;
-        else
-            return(num * factorialRecur(num - 1));
-    }
-
     public static long factorialT(int n) {
-        if (n<0) throw new IllegalArgumentException("Can't calculate factorial of negative");
-        return (n<2) ? 1 : n*factorialT(n-1);
+        if (n < 0) throw new IllegalArgumentException("Can't calculate factorial of negative");
+        return (n < 2) ? 1 : n * factorialT(n - 1);
     }
+
+//    public static int factorialRecur(int num) {
+//        Scanner sc = new Scanner(System.in);
+//        try {
+//            System.out.println("Enter and integer from 1 to 10: ");
+//            int numInput = sc.nextInt();
+//            sc.nextLine();
+//            System.out.println(numInput);
+//            if (numInput == 0)
+//                return 1;
+//            else
+//                return (numInput * factorialRecur(numInput - 1));
+//        } catch (NumberFormatException e) {
+//            e.getStackTrace();
+//        }
+//        return 0;
+
 
 
 }
+
 
 
